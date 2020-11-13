610969182b9448f54e17d47724a222ed71bf3204
Alexis Duque <alexisd61@gmail.com>
2020-11-13 20:14:01 +0100

    Publish

---
+++ js/sections.js
@@ -0,0 +1,46 @@
+$(function() {
+
+    $("#about").addClass('current');
+    $("#link-about").addClass('current');
+
+    //----------------------------------------------------------------------------	
+    // Change page "sections" when the menu item is clicked
+    //----------------------------------------------------------------------------	
+    $('#nav ul li a').click(function(){
+        var el = $(this);
+        if( !el.hasClass('current') ) {
+
+            // get the id of the section to show
+            var hash = $(this).attr('href');
+            var sec = hash.replace('#', '');
+            
+            window.location = hash;
+            
+            // hide the currently showing section
+            $('.section.current').slideUp(300, function(){
+                $(this).removeClass('current');
+                $('#' + sec).slideDown(300, function(){
+                    $(this).addClass('current');
+                });
+            });
+            $('#nav ul li a.current').removeClass('current');
+            el.addClass('current');
+        }
+        return false;
+    });
+      
+    
+    //------------------------------------------------------------------------------
+    // Show a page section based on the URL hash
+    //------------------------------------------------------------------------------
+    if( window.location.hash != '' ) {
+    
+        // see if there is a link ID to match our hash
+        var el = $(window.location.hash);
+        if( el.length > 0 ) {
+            el.click();
+        }
+    }
+    
+ 
+});
